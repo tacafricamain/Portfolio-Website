105d821e07d7c844f0d191fc1871c6b6e81df42a
githubn <tacafrica016@gmail.com>
2025-10-20 00:56:28 +0100

    kl

--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -4,49 +4,65 @@ import ProjectImg3 from "../../assets/project3.png";
 import ProjectImg4 from "../../assets/project4.png";
 import ProjectImg5 from "../../assets/project5.png";
 import ProjectImg6 from "../../assets/project6.png";
+import ProjectImg7 from "../../assets/project7.png";
+import ProjectImg8 from "../../assets/project8.png";
 
 const ProjectData = [
   {
     id: 1,
+    title: "Brahamas Tour",
+    description: "Discover the world, one journey at a time. A professional travel booking website featuring destination showcases, customer reviews, and tour packages with a 4.6 rating from 124,000+ reviews.",
+    image: ProjectImg8,
+    liveLink: "https://brahamas-tour.vercel.app/",
+  },
+  {
+    id: 2,
     title: "Tomato Delivery",
     description: "Tomato Delivery, a complete food ordering solution including a Customer Frontend, Admin Dashboard, and a Backend API.",
     image: ProjectImg1,
     liveLink: "https://tomato-delivery-frontend-upy2.onrender.com/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Chat Hive",
     description: "ChatHive is a realtime chat app with secure rooms, instant messaging, and theme support, built using WebSockets and React.",
     image: ProjectImg2,
     liveLink: "https://chathive-app.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Second Brain",
     description: "Second Brain is a personal link manager to save, organize and access links from Twitter, LinkedIn, YouTube, blogs, and more.",
     image: ProjectImg3,
     liveLink: "https://second-brainx.vercel.app/",
   },
-    {
-    id: 4,
+  {
+    id: 5,
     title: "Bozid Dental",
     description: "Bozid Dental is a modern dental clinic offering a wide range of services.",
     image: ProjectImg4,
     liveLink: "https://bozid-dental.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Giopat DC",
     description: "Giopat DC is a tech hub that uses modern technologies to manage solve problem and manage complex systems.",
     image: ProjectImg5,
     liveLink: "https://giopatdc.com.ng/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Joet Homes",
     description: "Joet Homes is a real estate platform offering properties for sale and rent including an Agent Frontend, Admin Dashboard, and a Backend API.",
     image: ProjectImg6,
     liveLink: "https://joethomes.com",
+  },
+  {
+    id: 8,
+    title: "Medical Dashboard",
+    description: "A comprehensive hospital management dashboard featuring patient tracking, appointment scheduling, staff management, and real-time analytics for healthcare professionals.",
+    image: ProjectImg7,
+    liveLink: "https://medic-0.vercel.app/dashboard",
   },
 ];
 
